496386f9c212fec9323cf15fe74832f56b683cb2
Callum booth <callum-booth@live.co.uk>
2019-01-29 18:49:40 +0000

    updated about me and job list

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,8 +16,23 @@ const jobData = [
     },
     {
         company: "Yarrington",
-        jobTitle: "Designer & Developer",
-        duration: "September 2010 - February 2015"
+        jobTitle: "Designer",
+        duration: "February 2013 -  February 2015"
+    },
+    {
+        company: "Yarrington",
+        jobTitle: "Digital Designer",
+        duration: "September 2012 - February 2013"
+    },
+    {
+        company: "Yarrington",
+        jobTitle: "Design Studio Assistant",
+        duration: "August 2010 - September 2012"
+    },
+    {
+        company: "FdA Graphic Design",
+        jobTitle: "Student",
+        duration: "September 2010 - September 2012"
     }
 ];
 
@@ -75,35 +90,19 @@ const About = () => {
                         <Box3 className="row">
                             <div className="desc col-6">
                                 <p>
-                                    I’m a hard working and enthusiastic developer with a passion
-                                    for front end development. I’m also have an FdA in graphic design 
-                                    and spent over 3 years experience working in a busy marketing agency 
-                                    along with 5 years development experience which has given me a 
-                                    strong set of skills to deliver high quality products.
+                                    I’m a hard working and enthusiastic developer with a passion for front end development. I have an FdA in graphic design and spent over 3 years experience working in a busy marketing agency along with 4 years development experience which has given me a strong set of skills to deliver high quality products.
                                 </p>
                                 <p>
-                                    I’m currently working with Moodle a learning management system, 
-                                    where I have helped bring projects from initial concept right through 
-                                    to development and release. Working both individually and as part 
-                                    of a team, not only bring products to customers, I have also helped 
-                                    implement tools to help with the day to day, such as incorporating 
-                                    version control systems (git) and task managers (gulp, grunt).
+                                    I’m currently working with Moodle a learning management system, where I have helped bring projects from initial concept right through to development and release. Working both individually and as part of a team, bringing products to customers, and implementing tools to help with the day to day tasks, such as incorporating version control systems (git) and task managers (gulp, grunt).
                                 </p>
                                 <p>
-                                    My skill set is primarily Javascript, PHP, MySQL along with HTML and CSS.
-                                    I have strong experience CSS preprocessors using SASS and have built 
-                                    projects using React, and Electron. I also have good understanding of 
-                                    REST API’s and have built a GraphQL middleware to improve data flow 
-                                    between multiple front and back end systems, along with integrating into 
-                                    Salesforce CRM and HubSpot marketing platform.
+                                    My skill set is primarily JavaScript, PHP, MySQL along with HTML and CSS. I have strong experience CSS preprocessors using SASS and have built projects using React, and Electron. I also have good understanding of REST API’s and have built a GraphQL API to improve data flow between multiple front and back end systems, along with integrating into Salesforce CRM and HubSpot marketing platform.
                                 </p>
                                 <p>
-                                    Outside of work I enjoy computer games, having been ranked in the 1%
-                                    for multiple games and photography both of which give me inspiration 
-                                    to create new ideas and deal with problems in alternate ways.
+                                    Outside of work I enjoy computer games, having been ranked in the 1% for multiple games and photography both of which give me inspiration to create new ideas and deal with problems in alternate ways.
                                 </p>
-                                <p> I’m currently working as full time developer but I’m looking for my 
-                                    next challenge in my career.
+                                <p> 
+                                    I’m currently working as full time developer based in Telford but I’m looking for a new challenge in my career.
                                 </p>
                             </div>
                             <div className="job-history col-6 mt-3">
